34d4a1dd64925538623af24b6ef2638936cceb3e
GimmeYerGold <jasongpoland@gmail.com>
2016-02-19 13:49:52 -0600

    initial commit

--- main.js
+++ main.js
@@ -1,22 +1,86 @@
 // GO!
 
+// <-------------#1------------>
+var vanishButton = document.querySelector("button")
+var navBar = document.querySelector(".nav-menu")
 
 
-// BONUS 
-var input = document.querySelector("#add-guest-bonus input")
-var guestList = document.querySelector("#add-guest-bonus ul")
+var navBarShowing = true
 
-var addGuest = function(e) {
-	console.log(e.keyCode)
-	if (e.keyCode === 13) {
-		var newGuestItem = document.createElement('li')
-		newGuestItem.innerHTML = e.target.value + "<button>X</button>"
-		newGuestItem.classList.add('guest')
-		newGuestItem.querySelector('button').addEventListener('click',function() {
-			guestList.removeChild(newGuestItem)
-		})
-		guestList.appendChild(newGuestItem)
-		e.target.value = ''
-	}
+
+
+var vanisher = function(){
+if (navBarShowing) {
+	navBar.style.opacity = 0
+	navBarShowing = false
+	vanishButton.textContent = "show nav"
+
 }
-input.addEventListener('keypress',addGuest)
+else {
+	navBar.style.opacity = 1
+	navBarShowing = true
+	vanishButton.textContent = "hide nav"
+}
+}
+vanishButton.addEventListener("click", vanisher)
+
+// <-------------#2------------>
+
+var inputEl = document.querySelector('input[type="text"]')
+
+var guestListUl = document.querySelector(".guest-list")
+
+var addGuest = function(keyEvent) {
+
+  var inputEl = keyEvent.srcElement
+  if (keyEvent.keyCode === 13) {
+      var userInput = inputEl.value
+      var newLi = document.createElement('li')
+      newLi.className = "guest"
+      newLi.textContent = userInput
+      guestListUl.appendChild(newLi)
+      inputEl.value = ''
+  }  
+}
+inputEl.addEventListener('keydown',addGuest)
+// BONUS 
+
+
+
+var inputBonusEl = document.querySelector('input[type="text-bonus"]')
+
+var guestListBonusUl = document.querySelector(".guest-list-bonus")
+
+
+
+var addGuestBonus = function(keyEvent) {
+	console.log(keyEvent)
+	var inputBonusEl = keyEvent.srcElement
+	if (keyEvent.keyCode === 13) {
+
+		var userBonusInput = inputBonusEl.value
+		var newBonusLi = document.createElement('li') 
+		var newBonusCheckBox = document.createElement("BUTTON")
+		newBonusCheckBox.textContent = "X"
+		newBonusLi.className += "guest"
+		newBonusLi.textContent = userBonusInput
+		guestListBonusUl.appendChild(newBonusLi)
+		newBonusLi.appendChild(newBonusCheckBox)
+		newBonusCheckBox.addEventListener('click',function(){
+		guestListBonusUl.removeChild(newBonusLi)
+})
+		inputBonusEl.value = ''	
+
+	}
+
+
+}
+
+
+
+inputBonusEl.addEventListener('keydown',addGuestBonus)
+
+
+
+
+
